b9477a023dc3d9dceba20ecef29cdffad3943f51
JasonChong96 <jasontjakra@gmail.com>
2019-01-15 07:25:46 -0800

    Implement Queue and Stack

--- DataStructures/Queue.swift
+++ DataStructures/Queue.swift
@@ -7,47 +7,54 @@
  - Date: 2018
  */
 struct Queue<T> {
+    
+    var items: [T] = []
 
     /// Adds an element to the tail of the queue.
     /// - Parameter item: The element to be added to the queue
     mutating func enqueue(_ item: T) {
+        items.append(item)
     }
 
     /// Removes an element from the head of the queue and return it.
     /// - Returns: item at the head of the queue
     mutating func dequeue() -> T? {
-        // TODO: Replace/remove the following line in your implementation.
-         return nil
+        if isEmpty {
+            return nil
+        }
+        
+        return items.removeFirst()
     }
 
     /// Returns, but does not remove, the element at the head of the queue.
     /// - Returns: item at the head of the queue
     func peek() -> T? {
-        // TODO: Replace/remove the following line in your implementation.
-         return nil
+        if isEmpty {
+            return nil
+        }
+        
+        return items[0]
     }
 
     /// The number of elements currently in the queue.
     var count: Int {
-        // TODO: Replace/remove the following line in your implementation.
-         return 0
+        return items.count
     }
 
     /// Whether the queue is empty.
     var isEmpty: Bool {
-        // TODO: Replace/remove the following line in your implementation.
-        return false
+        return items.isEmpty
     }
 
     /// Removes all elements in the queue.
     mutating func removeAll() {
+        items = []
     }
 
     /// Returns an array of the elements in their respective dequeue order, i.e.
     /// first element in the array is the first element to be dequeued.
     /// - Returns: array of elements in their respective dequeue order
     func toArray() -> [T] {
-        // TODO: Replace/remove the following line in your implementation.
-        return [T]()
+        return items
     }
 }
